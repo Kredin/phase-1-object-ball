9caba31fee7d80605950b076abf6d519d933e400
Blade <bladeprojectz@gmail.com>
2024-02-06 18:39:33 +0800

    lab complete, bonus and super bonus completed

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -117,7 +117,219 @@ function gameObject() {
     return info;
 }
 
-console.log(gameObject())
 
+function homeTeamName() {
+    return gameObject()["home"]["teamName"];
+  }
+  
+  console.log(homeTeamName());
+
+/* fixed code below */
+
+function numPointsScored(player) {
+    console.log("is working")
+    if(gameObject()["home"]["players"][player]){
+        return gameObject()["home"]["players"][player]["points"]
+    }
+    return gameObject()["away"]["players"][player]["points"]
+    
+    
+        
+    
+    // for(let char in gameObject()["away"]["players"]) {
+    //     if(char === player){
+    //         return gameObject()["away"]["players"][player]["points"]
+    //     }
+    // }
+}
+console.log(numPointsScored("Alan Anderson"));
+
+
+function shoeSize(player) {
+    for(let char in gameObject()["home"]["players"]) {
+        if(char === player){
+            return gameObject()["home"]["players"][player]["shoe"]
+        }
+    }
+    for(let char in gameObject()["away"]["players"]) {
+        if(char === player){
+            return gameObject()["away"]["players"][player]["shoe"]
+        }
+    }
+}
+console.log(shoeSize("Brendan Haywoon"));
+
+
+function teamColors(value) {
+    if(value === gameObject()["home"]["teamName"]) {
+        return gameObject()["home"]["colors"]
+    }else if(value === gameObject()["away"]["teamName"]) {
+        return gameObject()["away"]["colors"]
+    }else {
+        return "Invalid Team Name"
+    }
+}
+console.log(teamColors("Charlotte Hornets"))
+
+
+function teamNames() {
+    return [gameObject()["home"]["teamName"], gameObject()["away"]["teamName"]]
+}
+console.log(teamNames())
+
+
+function playerNumbers(value) {
+    if(value === gameObject()["home"]["teamName"]) {
+        let nums = []
+        for(let i in gameObject()["home"]["players"]) {
+            nums.push(gameObject()["home"]["players"][i]["number"])
+        }
+        return nums
+    }if(value === gameObject()["away"]["teamName"]) {
+        let nums = []
+        for(let i in gameObject()["away"]["players"]) {
+            nums.push(gameObject()["away"]["players"][i]["number"])
+        }
+        return nums
+    }else {
+        return "Invalid Team Name"
+    }
+}
+console.log(playerNumbers('Charlotte Hornets'))
+
+
+function playerStats(player) {
+    for(let char in gameObject()["home"]["players"]) {
+        if(char === player){
+            return gameObject()["home"]["players"][player]
+        }
+    }
+    for(let char in gameObject()["away"]["players"]) {
+        if(char === player){
+            return gameObject()["away"]["players"][player]
+        }
+    }
+}
+
+console.log(playerStats("Mason Plumlee"))
+
+
+function bigShoeRebounds() {
+    let bigShoe = 0
+    let player = ""
+    let rebounds = 0
+    for(let char in gameObject()["home"]["players"]) {
+        if(bigShoe < gameObject()["home"]["players"][char]["shoe"]){
+            bigShoe = gameObject()["home"]["players"][char]["shoe"]
+            player = char
+            rebounds = gameObject()["home"]["players"][char]["rebounds"]
+        }
+    }
+    for(let char in gameObject()["away"]["players"]) {
+        if(bigShoe < gameObject()["away"]["players"][char]["shoe"]){
+            bigShoe = gameObject()["away"]["players"][char]["shoe"]
+            player = char
+            rebounds = gameObject()["away"]["players"][char]["rebounds"]
+        }
+    }
+    console.log(typeof(player))
+    return bigShoe + " " + player + " " + rebounds;
+}
+
+console.log(bigShoeRebounds())
+
+
+function mostPointsScored() {
+    let mvp = 0
+    let player = ""
+    for(let char in gameObject()["home"]["players"]) {
+        if(mvp < gameObject()["home"]["players"][char]["points"]){
+            mvp = gameObject()["home"]["players"][char]["points"]
+            player = char
+        }
+    }
+    for(let char in gameObject()["away"]["players"]) {
+        if(mvp < gameObject()["away"]["players"][char]["points"]){
+            mvp = gameObject()["away"]["players"][char]["points"]
+            player = char
+        }
+    }
+    return player + " " + mvp;
+}
+
+console.log(mostPointsScored())
+
+
+function winningTeam() {
+    let homeScore = 0
+    let awayScore = 0
+    for(let char in gameObject()["home"]["players"]) {
+        homeScore = homeScore + gameObject()["home"]["players"][char]["points"]
+        }
+    for(let char in gameObject()["away"]["players"]) {
+        awayScore = awayScore + gameObject()["away"]["players"][char]["points"]
+        }
+    
+    if(homeScore > awayScore) {
+        return gameObject()["home"]["teamName"]
+    }else if(awayScore > homeScore) {
+        return agameObject()["away"]["teamName"]
+    }else {
+        return "Tie"
+    }
+    
+}
+
+console.log(winningTeam())
+
+
+function playerWithLongestName() {
+    let player = "a"
+    for(let char in gameObject()["home"]["players"]){
+        if(player.length < char.length) {
+            player = char
+        }
+    }
+    for(let char in gameObject()["away"]["players"]){
+        if(player.length < char.length) {
+            player = char
+        }
+    }
+    return player
+}
+
+console.log(playerWithLongestName())
+
+
+function doesLongNameStealATon() {
+    let player = "a"
+    let steals = 0
+    for(let char in gameObject()["home"]["players"]){
+        if(player.length < char.length) {
+            player = char
+            steals = gameObject()["home"]["players"][char]["steals"]
+        }
+    }
+    for(let char in gameObject()["away"]["players"]){
+        if(player.length < char.length) {
+            player = char
+            steals = gameObject()["away"]["players"][char]["steals"]
+        }
+    }
+
+    for(let char in gameObject()["home"]["players"]){
+        if(steals < gameObject()["home"]["players"][char]["steals"]) {
+            return false
+        }
+    }
+    for(let char in gameObject()["away"]["players"]){
+        if(steals < gameObject()["away"]["players"][char]["steals"]) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(doesLongNameStealATon())
 
 
